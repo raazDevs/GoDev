2465f207a8f42f12b22fa05b7bd4c109b770bd9c
Rajdeep Nath <nathsaju123@gmail.com>
2026-05-23 20:16:24 +0530

    If-Else Statement

---
+++ If_Else/main.go
@@ -0,0 +1,61 @@
+package main
+
+func main() {
+
+	// age:= 18
+	
+	// if age >= 18 {
+	// 	fmt.Println("You are an adult.")
+	// }else if age >= 13 {
+	// 	fmt.Println("You are a teenager.")	
+	
+	// }else if age >= 0 {
+	// 	fmt.Println("You are a child.")
+	// }else {
+	// 	fmt.Println("Invalid age.")
+	// }
+
+	// var role ="admin"
+	// var accessLevel = true
+
+	// if role == "admin" || accessLevel {
+	// 	fmt.Println("Access granted.")
+	// }
+
+
+	// var role ="admin"
+	// var accessLevel = false
+
+	// if role == "admin" && accessLevel {
+	// 	fmt.Println("Access granted.")
+	// } else{
+	// 	fmt.Println("Access denied.")
+	// }
+
+
+
+// "&" is the logical AND operator, which returns true if both conditions are true. In the example, access is granted only if the role is "admin" and the accessLevel is true.
+// "||" is the logical OR operator, which returns true if at least one of the conditions is true. In the example, access is granted if either the role is "admin" or the accessLevel is true.
+
+
+
+
+//We can declare a variable inside the if statement, and it will only be accessible within that block. For example:
+
+// if age := 18; age >= 25 {
+//     fmt.Println("You are an adult.")
+// } else if age >= 13 {
+//     fmt.Println("You are a teenager.")
+// } else if age >= 0 {
+//     fmt.Println("You are a child.")
+// } else {
+//     fmt.Println("Invalid age.")
+// }	
+
+
+	// go does not have a ternary operator like some other languages (e.g., ? : in C or Java). Instead, you can achieve similar functionality using an if-else statement. For example:
+
+	// var age = 18
+	// var category string	
+
+}
